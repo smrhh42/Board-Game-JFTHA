2eb91306d5beca10d7ef028e2aed2381f8bd49f8
tua84076 <tua84076@CIS-WCC20919.tu.temple.edu>
2014-03-19 12:37:35 -0400

    added discount method and atkEnemy conditions

--- src/jftha/heroes/Merchant.java
+++ src/jftha/heroes/Merchant.java
@@ -2,8 +2,11 @@ package jftha.heroes;
 
 import jftha.items.WoolClothing;
 import jftha.items.WagonOfGoods;
+import jftha.main.*;
 
 public class Merchant extends Hero{
+    
+    final double discount = 0.25;
     
     //Constructor
     public Merchant(){
@@ -14,8 +17,20 @@ public class Merchant extends Hero{
         this.setDefense(super.getDefense() - 2);
         this.setLuck(super.getLuck() + 5);
         this.addItem(new WoolClothing());
-        this.addItem(new WagonOfGoods());
-        
+        this.addItem(new WagonOfGoods());     
     }
     
+    @Override
+    public boolean buy(Buyable buy){
+        int f = buy.getGoldCost(); 
+        f -= (buy.getGoldCost() * discount);
+        //need setter and getter for gold
+        //this.getGold() - f;
+        return false;
+    }
+    
+    @Override
+    public void atkEnemy(){
+        //Can only perform attacks with items and spells.
+    }
 }
