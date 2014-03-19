186aab71fe584cef12a3c0c6d669f81c22d8fa32
Shane Riggins <tuc33380@temple.edu>
2014-03-19 15:07:30 -0400

    Added Javadoc comments. Fixed addItem() and addSpell() to not allow duplicates.

--- src/jftha/heroes/Hero.java
+++ src/jftha/heroes/Hero.java
@@ -6,38 +6,43 @@ package jftha.heroes;
 
 import java.util.ArrayList;
 import jftha.items.Item;
+import jftha.main.Buyable;
 import jftha.spells.Spell;
 import jftha.main.Effect;
 
 public class Hero {
-    //Determines how much damage can be dealt to an enemy through weapons
+    // Determines how much damage can be dealt to an enemy through weapons
     private int strength;
-    //Determines how many spaces the player can move per turn
+    // Determines how many spaces the player can move per turn
     private int agility;
-    //Determines how many MP(magic points) the player has, what spells the player can cast,
-    //and how much damage can be dealt to an enemy
+    // Determines how many MP(magic points) the player has, what spells the player can cast,
+    // and how much damage can be dealt to an enemy
     private int magic;
-    //Determines how much HP(health point) a player has and how much damage a player can endure
+    // Determines how much HP(health point) a player has and how much damage a player can endure
     private int defense;
-    //Affect all skills by a little bit
+    // Affect all skills by a little bit
     private int luck;
-    //Determines how many items a player can hold at any time
+    // Determines how many items a player can hold at any time
     private int storage_space;
-    //The items the Hero is currently carrying
+    // The items the Hero is currently carrying
     private ArrayList<Item> items;
-    //Determines how many spells a player is able to cast
+    // Determines how many spells a player is able to cast
     private int spell_slots;
-    //The spells the Hero currently knows
+    // The spells the Hero currently knows
     private ArrayList<Spell> spells;
-    //Determined by Defense stat. Also known as health points
+    // Determined by Defense stat. Also known as health points
     private int maxHP;
+    // Health the player currently has
     private int currentHP;
-    //Determined by Magic stat. Also known as magic points
+    // Determined by Magic stat. Also known as magic points
     private int maxMP;
+    // Magic the player currently has
     private int currentMP;
-    //Special to be determined by the defending class
+    // Special to be determined by the defending class
     private Effect special; //***Should this be an Effect for an entirely new class?***
-    
+    // Gold the player currently has
+    private int gold;
+    // Determines if player is a ghost
     private boolean isGhost;
     
     //Constructor
@@ -54,6 +59,7 @@ public class Hero {
         this.items = new ArrayList<>(this.storage_space);
         this.spells = new ArrayList<>(this.spell_slots);
         this.isGhost = false;
+        this.gold = 0;
     }
     
     //Setter methods
@@ -80,11 +86,23 @@ public class Hero {
         this.spell_slots = spell_slots;
         this.spells.ensureCapacity(spell_slots);
     }
-    public void setHP(int hp){
+    public void setMaxHP(int hp){
         this.maxHP = hp;
     }
-    public void setMP(int mp){
+    public void setCurrentHP(int hp) {
+        this.currentHP = hp;
+    }
+    public void setMaxMP(int mp){
         this.maxMP = mp;
+    }
+    public void setCurrentMP(int mp) {
+        this.currentMP = mp;
+    }
+    public void addGold(int gold) {
+        this.gold += gold;
+    }
+    public void spendGold(int gold) {
+        this.gold -= gold;
     }
     
     //Getter Methods
@@ -109,17 +127,33 @@ public class Hero {
     public int getSpellSlots(){
         return spell_slots;
     }
-    public int getHP(){
+    public int getMaxHP(){
         return maxHP;
     }
-    public int getMP(){
+    public int getcurrentHP() {
+        return currentHP;
+    }
+    public int getMaxMP(){
         return maxMP;
     }
-    
-    public void castSpell(){
-        
+    public int getCurrentMP() {
+        return currentMP;
+    }
+    public int getGold() {
+        return gold;
     }
     
+    
+    /** Allows a character to cast a spell.
+     * 
+     * @param spell The spell to be cast.
+     */
+    public void castSpell(Spell spell){
+        
+    }
+    /** Turns a character into a ghost.
+     * 
+     */
     public void makeGhost(){
         this.isGhost = true;
         //Cannot hold items, except spiritual items (see Items).
@@ -138,30 +172,71 @@ public class Hero {
         //Note: Only certain items, such as spirit weapons, can drain SE.
     }
     
+    /** Restores a character from ghost state.
+     * 
+     */
     public void unGhost(){
         this.isGhost = false;
         this.currentHP = this.maxHP;
         //Lose Spectre Shots;
     }
     
-    public void atkEnemy(){
-        //This player's turn
+    /** Allows a character to attack another character
+     * 
+     * @param attacked The character that is getting attacked
+     */
+    public void attackEnemy(Hero attacked){
         //cannot atk ghost unless this character have spiritual item
-        //This player's action
-        //
+        double damage = (this.strength - attacked.defense) - (0.2 * (this.luck - attacked.luck));
         
     }
-    //
+    /** Allows a character to buy an item in the store.
+     * 
+     * @param buy The Buyable item that the player want to purchase
+     * @return true if purchase is went through
+     */
+    public boolean buy(Buyable buy){
+        int f = getGold(); 
+        // if character has the gold
+        f -= (buy.getGoldCost());
+        //need setter and getter for gold
+        //this.getGold() - f;
+        return false;
+    }
+    
+    /**Adds an item to character's inventory. Will only add item if:
+     *  <\t>1) The character does not already have this item
+     *  <\t>2) The character has storage space for the item
+     * 
+     * @param item The item to be added to the character's inventory
+     * @return true if item was added
+     */
     public boolean addItem(Item item) {
-        if(!items.contains(item) && items.size() < storage_space) {
+        for(Item i : items) {
+            if(i.getClass().equals(item.getClass()))
+                return false;
+        }
+        
+        if(items.size() < storage_space) {
             if(items.add(item))
                 return true;
         }
         return false;
     }
     
+    /** Adds a spell to character's spells. Will only add spell if:
+     *  <\t>1) The character does not already know this spell
+     *  <\t>2) The character has a spell slot for the spell 
+     * 
+     * @param spell Spell to be added to the character
+     * @return true if spell was added
+     */
     public boolean addSpell(Spell spell) {
-        if(!spells.contains(spell) && spells.size() < spell_slots) {
+        for(Spell s : spells) {
+            if(s.getClass().equals(spell.getClass()))
+                return false;
+        }
+        if(spells.size() < spell_slots) {
             if(spells.add(spell))
                 return true;
         }
