17a82f1f8fe342c0462cca5a9101fb791360f7f4
tua84076 <tua84076@CIS-WCC20919.tu.temple.edu>
2014-03-19 12:16:52 -0400

    added makeGhost() and unGhost() method and conditions.

--- src/jftha/heroes/Hero.java
+++ src/jftha/heroes/Hero.java
@@ -30,11 +30,15 @@ public class Hero {
     //The spells the Hero currently knows
     private ArrayList<Spell> spells;
     //Determined by Defense stat. Also known as health points
-    private int hp;
+    private int maxHP;
+    private int currentHP;
     //Determined by Magic stat. Also known as magic points
-    private int mp;
+    private int maxMP;
+    private int currentMP;
     //Special to be determined by the defending class
     private Effect special; //***Should this be an Effect for an entirely new class?***
+    
+    private boolean isGhost;
     
     //Constructor
     public Hero(){
@@ -45,10 +49,11 @@ public class Hero {
         this.luck = 10;
         this.storage_space = 5;
         this.spell_slots = 2;
-        this.hp = 60;
-        this.mp = 30;
+        this.maxHP = 60;
+        this.maxMP = 30;
         this.items = new ArrayList<>(this.storage_space);
         this.spells = new ArrayList<>(this.spell_slots);
+        this.isGhost = false;
     }
     
     //Setter methods
@@ -76,10 +81,10 @@ public class Hero {
         this.spells.ensureCapacity(spell_slots);
     }
     public void setHP(int hp){
-        this.hp = hp;
+        this.maxHP = hp;
     }
     public void setMP(int mp){
-        this.mp = mp;
+        this.maxMP = mp;
     }
     
     //Getter Methods
@@ -105,12 +110,47 @@ public class Hero {
         return spell_slots;
     }
     public int getHP(){
-        return hp;
+        return maxHP;
     }
     public int getMP(){
-        return mp;
+        return maxMP;
     }
     
+    public void castSpell(){
+        
+    }
+    
+    public void makeGhost(){
+        this.isGhost = true;
+        //Cannot hold items, except spiritual items (see Items).
+        this.items.clear(); // Need to check for spiritual items
+        //Regen MP
+        this.currentMP = this.maxMP;        
+        //Cannot be hit, except by spiritual items or items/spells with Phantom Pain ability.
+        
+        //Can cast only Spectre Shot spell (cost 1 SE).
+        this.spells.clear(); //save current spell in another arraylist incase player comes back;
+        this.spells.add(null); // spectre shot;
+        
+        //If killed again (all of MP depleted), you are eliminated from the game.
+        //if(isEliminated = true){ lose turn phase};
+        
+        //Note: Only certain items, such as spirit weapons, can drain SE.
+    }
+    
+    public void unGhost(){
+        this.isGhost = false;
+        this.currentHP = this.maxHP;
+        //Lose Spectre Shots;
+    }
+    
+    public void atkEnemy(){
+        //This player's turn
+        //cannot atk ghost unless this character have spiritual item
+        //This player's action
+        //
+        
+    }
     //
     public boolean addItem(Item item) {
         if(!items.contains(item) && items.size() < storage_space) {
