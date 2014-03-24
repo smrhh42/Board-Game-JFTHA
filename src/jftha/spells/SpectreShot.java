8f38b1d4b918d6196158608a1cfdf27db87a6184
Shane Riggins <tuc33380@temple.edu>
2014-03-24 10:08:04 -0400

    Skeleton SpectreShot class.

---
+++ src/jftha/spells/SpectreShot.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package jftha.spells;
+
+/**
+ *
+ * @author shane
+ */
+public class SpectreShot extends Spell {
+    
+}
