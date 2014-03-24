22d07f813a9f5a8ed0e8ffb940d601f00e5441bb
Shane Riggins <tuc33380@temple.edu>
2014-03-23 20:57:40 -0400

    Override of attackEnemy with 2x attack strength

--- src/jftha/heroes/Barbarian.java
+++ src/jftha/heroes/Barbarian.java
@@ -13,11 +13,34 @@ public class Barbarian extends Hero{
         this.addItem(new Ax());
     }
     
-    public void ifAtked(){
+    @Override
+    public void attackEnemy(Hero enemy) {
+        int strength = this.getStrength();
         Random rand = new Random();
-        if((rand.nextInt(5) + 1) == 1){
-            //attack back with 2x strength
+        if(wasAttacked()) {
+            if((rand.nextInt(5) + 1) == 1)
+                strength *= 2;
         }
+        if (enemy.wasAttacked() == false) { // **?? Can only get attacked once per turn?
+            if (enemy.isGhost() == false) { //cannot attack ghost unless under certain circumstances
+
+                int randomDamage = rand.nextInt(3);
+                double damage = (strength - enemy.getDefense()) - (0.2 * (this.getLuck() - enemy.getLuck())) + randomDamage;
+                if (damage < 0) { //attacker sucks
+                    damage = 0;
+                }
+                int intDamage = (int) Math.round(damage);
+//                System.out.println(intDamage + " inflicted by " + this + " to " + enemy + ".");
+                enemy.setCurrentHP(enemy.getCurrentHP()- intDamage);
+                if (enemy.getCurrentHP() <= 0){
+                    enemy.makeGhost();
+                }
+            } else { //attacking ghost
+                //handle spiritual items
+                //if no spiritual items, the Attack phase is skipped
+            }
+        }
+        enemy.setWasAttacked(true);
     }
 }
 
