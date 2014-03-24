1adcea38517ee7a584b4f4ae1cb4d46c01582634
Shane Riggins <tuc33380@temple.edu>
2014-03-23 21:03:57 -0400

    Override of buy with discount. Still need to override attackEnemy

--- src/jftha/heroes/Merchant.java
+++ src/jftha/heroes/Merchant.java
@@ -20,15 +20,19 @@ public class Merchant extends Hero {
         this.addItem(new WagonOfGoods());     
     }
     
-    /**
+    /** Merchant version of buy that gives a discount for every purchase.
      * 
-     * @param buy
-     * @return 
+     * @param buy The item being bought.
+     * @return True if the purchase went through
      */
     @Override
     public boolean buy(Buyable buy){
-        if(getGold() >= buy.getGoldCost()) {
-            spendGold((int)(buy.getGoldCost() * discount));
+        int currentGold = this.getGold();
+        //If character has the gold
+        if(currentGold >= buy.getGoldCost()){
+            double gold = currentGold - (buy.getGoldCost() * discount);
+            currentGold = (int) Math.round(gold);
+            this.setGold(currentGold);
             return true;
         }
         return false;
@@ -36,10 +40,10 @@ public class Merchant extends Hero {
     
     /**
      * 
-     * @param attacked 
+     * @param enemy
      */
     @Override
-    public void attackEnemy(Hero attacked){
+    public void attackEnemy(Hero enemy){
         //Can only perform attacks with items and spells.
     }
 }
