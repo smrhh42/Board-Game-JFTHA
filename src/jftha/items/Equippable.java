5be5e6fcaa00891e1e06d16e317d090e3483d847
Shane Riggins <tuc33380@temple.edu>
2014-03-23 21:21:15 -0400

    Skeleton of Equippable class.

---
+++ src/jftha/items/Equippable.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package jftha.items;
+
+/**
+ *
+ * @author shane
+ */
+public class Equippable extends Item {
+    //a Hero can equip only one weapon and one armor <-- should this be in the Hero class?
+    //An equippable item affects only the stats of the Hero who has the items equipped
+}
